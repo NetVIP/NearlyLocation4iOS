d4c19fceb6ce25a5c56033e689f47549ad1470fa
LunaGao <gao.xue.qi@qq.com>
2016-04-20 10:02:51 +0800

    init project

---
+++ NearlyLocation4iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NearlyLocation4iOS
+//
+//  Created by Luna Gao on 16/4/20.
+//  Copyright © 2016年 luna.gao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NearlyLocation4iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NearlyLocation4iOS
+//
+//  Created by Luna Gao on 16/4/20.
+//  Copyright © 2016年 luna.gao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
